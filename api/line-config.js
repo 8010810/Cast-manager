788d447533516e6abb8c4668ad71dc6062e4c088
8010810 <yamatotakuma.0810@gmail.com>
2026-04-30 02:13:03 +0900

    Update line-config.js

--- api/line-config.js
+++ api/line-config.js
@@ -1,8 +1,6 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
-res.setHeader(‘Access-Control-Allow-Methods’, ‘GET, POST, OPTIONS’);
-if (req.method === ‘OPTIONS’) return res.status(200).end();
 res.status(200).json({
 channelId: process.env.LINE_CHANNEL_ID || ‘’
 });
-};
+}
